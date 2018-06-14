5a70e83b288f721bf96460e9dd90e6c059b104f9
YoavOrlev <yoav.orlev@milab.idc.ac.il>
2018-06-14 16:25:32 +0300

    boosting_hyperparametrs : A few renaming

--- src/boosting/boosting_hyperparameters.py
+++ src/boosting/boosting_hyperparameters.py
@@ -1,29 +1,29 @@
 class GBRTHyperparameters(object):
-    number_of_trees = 1
-    tree_width = 1
-    tree_depth = 1
-    nu = 1
+    num_trees = 1
+    min_node_size = 1
+    max_depth = 1
+    shrinkage = 1
     num_threshold = 1
     sample_size = 1
 
-    def __init__(self, num_of_tress, tree_width, tree_depth, nu, num_threshold, sample_size):
-        self.number_of_trees = num_of_tress
-        self.tree_width = tree_width
-        self.tree_depth = tree_depth
-        self.nu = nu
+    def __init__(self, num_trees, min_node_size, max_depth, shrinkage, num_threshold, sample_size):
+        self.num_trees = num_trees
+        self.tree_width = min_node_size
+        self.max_depth = max_depth
+        self.shrinkage = shrinkage
         self.num_threshold = num_threshold
         self.sample_size = sample_size
 
     def get_cart_hyperparameters(self):
-        return CARTHyperparameters(self.tree_width, self.tree_depth, self.num_threshold)
+        return CARTHyperparameters(self.tree_width, self.max_depth, self.num_threshold)
 
 
 class CARTHyperparameters(object):
-    tree_width = 0
-    tree_depth = 0
+    min_node_size = 0
+    max_depth = 0
     num_threshold = 0
 
-    def __init__(self, tree_width, tree_depth, num_threshold):
-        self.tree_width = tree_width
-        self.tree_depth = tree_depth
+    def __init__(self, min_node_size, max_depth, num_threshold):
+        self.min_node_size = min_node_size
+        self.max_depth = max_depth
         self.num_threshold = num_threshold
