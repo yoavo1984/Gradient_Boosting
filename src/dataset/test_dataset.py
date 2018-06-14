4f0d80f3aa07e30dcbb0a2b6ea2b4e2eed14ec9b
koren88i <koren.maliniak@gmail.com>
2018-06-14 20:34:53 +0300

    fixed string values imputation

--- src/dataset/test_dataset.py
+++ src/dataset/test_dataset.py
@@ -1,5 +1,6 @@
 from src.dataset.dataset import Dataset
-
+import re
+import numpy as np
 
 class TestDataset(Dataset):
     def __init__(self, data_subset, target_name,  coding_map, imputation_map):
@@ -8,9 +9,16 @@ class TestDataset(Dataset):
 
     def code_categorial_features(self, coding_map):
         obj_cols = self.dataframe.select_dtypes(include=['object']).columns
-
         for col in obj_cols:
             self.dataframe[col].replace(coding_map[col], inplace=True)
+            # self.dataframe[col] = self.dataframe[col].map(coding_map[col])
+            self.dataframe[col]=self.dataframe[col].fillna("string")
+            str_value = re.compile('.')
+            self.dataframe[col] = self.dataframe[col].replace(to_replace=str_value, value=np.nan)
+            col_mean=self.dataframe[col].mean()
+            self.dataframe[col] = self.dataframe[col].replace(to_replace=str_value, value=col_mean)
+            # self.dataframe.dropna()
+            pass
 
     def mean_imputation(self, imputation_map):
         self.dataframe.fillna(imputation_map, inplace=True)
