bdec4e62eb59c22a2b16491784208e553ce553ce
YoavOrlev <yoav.orlev@milab.idc.ac.il>
2018-06-14 17:00:09 +0300

    main : Can now parse a configuration file.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
+from src.boosting.boosting_hyperparameters import GBRTHyperparameters
+from src.boosting.gradient_boosting import gbrt
 from src.dataset.dataset_loader import create_data
 
 path_to_file = "data"
 
+def parse_configuration_file():
+    with open("configuration") as configuration_file:
+        lines = configuration_file.readlines()
+        configuration = lines[1].split(", ")
+
+    return GBRTHyperparameters(*configuration,)
+
+
+
 if __name__ == "__main__":
     train, test = create_data(path_to_file)
-    print (train.dataframe.info())
-    print (test.dataframe.info())
+    hyperparameters = parse_configuration_file()
+    gbrt(train, hyperparameters, test)
