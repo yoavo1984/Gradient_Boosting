fd5905ccf4638b05c300be032987701b56e11575
YoavOrlev <yoav.orlev@milab.idc.ac.il>
2018-06-19 16:52:00 +0300

    Deliverable Plotting : File to generate plotting for deliverable data.

--- src/deliverable/deliverable_plotting.py
+++ src/deliverable/deliverable_plotting.py
@@ -5,16 +5,20 @@ import numpy as np
 
 def plot_depth_deliverable():
     # Load pickle data object
-    with open('depth_deliverable.pickle', 'rb') as handle:
+    with open('deliverable/deliverable_raw_data/depth_deliverable.pickle', 'rb') as handle:
         data = pickle.load(handle)
 
     # Plot depth vs error
     for value, errors in data.items():
         num_of_trees = np.arange(len(errors))
 
-        plt.title("{}".format(value))
+        plt.title("Tree Depth = {}".format(value))
+
+        plt.xlabel("Number of trees")
+        plt.ylabel("RMSE")
         plt.plot(num_of_trees, errors)
         plt.show()
+        plt.savefig("{}_depth.png".format(value))
         plt.clf()
 
 
@@ -30,16 +34,19 @@ def plot_sampling_deliverable():
     for val in time_data:
         values.append(val[0])
         training_time.append(val[1])
-    plt.title("sampling vs time")
+    plt.title("Sampling vs Time")
     plt.plot(values, training_time)
+    plt.savefig("sampling_time.png")
     plt.clf()
 
     for value, errors in data.items():
         num_of_trees = np.arange(len(errors))
 
-        plt.title("{}".format(value))
+        plt.xlabel("Number of Trees")
+        plt.ylabel("RMSE")
+        plt.title("Sampling Portion = {}".format(value))
         plt.plot(num_of_trees, errors)
-        plt.show()
+        plt.savefig("{}_threshold.png".format(value))
         plt.clf()
 
 
@@ -55,13 +62,17 @@ def plot_threshold_deliverable():
     for val in time_data:
         values.append(val[0])
         training_time.append(val[1])
-    plt.title("threshold vs time")
+    plt.title("Threshold vs Time")
     plt.plot(values, training_time)
+    plt.savefig("threshold_time.png")
     plt.clf()
 
     for value, errors in data.items():
         num_of_trees = np.arange(len(errors))
 
+        plt.xlabel("Number of Trees")
+        plt.ylabel("RMSE")
+        plt.title("Sampling Portion = {}".format(value))
         plt.title("{}".format(value))
         plt.plot(num_of_trees, errors)
         plt.show()
